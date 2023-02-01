3f5f1a46e30c8d12e6027cfcfa0acaca41563cc2
HiradFakouri <hiradfakouri200@gmail.com>
2023-02-01 21:21:32 +0000

    added auth removed encryption for now

--- app.py
+++ app.py
@@ -1,16 +1,31 @@
-from flask import Flask, render_template, request, redirect, flash
+from flask import Flask, render_template, request, redirect, flash, session
 import pymongo
 import os
 from dotenv import load_dotenv
-from cryptography.fernet import Fernet
+
+###########  Encryption ###########
+"""
+from Crypto.Random import get_random_bytes
+from Crypto.Protocol.KDF import PBKDF2
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import pad, unpad
+"""
+
+#encryption - have to work on it later
+#salt = os.getenv('salt')
+#password = os.getenv('password')
+
+#key = PBKDF2(password, salt, dkLen=32)
+
+#cipher = AES.new(key, AES.MODE_CBC)
+#hashedPassword = cipher.encrypt(pad(password.encode("utf-8"), AES.block_size))
+#decryptedPassword = unpad(cipher.decrypt(datapassword), AES.block_size)
+
+###########  Encryption ###########
 
 #loading the mongodb_url
 load_dotenv()
 mongo_url = os.getenv('mongo_url')
-
-#config fernet
-key = Fernet.generate_key()
-fernet = Fernet(key)
 
 #connecting to database
 cluster = pymongo.MongoClient(mongo_url)
@@ -26,9 +41,16 @@ app.config['SECRET_KEY'] = 'secret_key'
 def index():
     return render_template('index.html')
 
-@app.route('/todo')
+@app.route('/todo', methods=['GET', 'POST'])
 def todo():
-    return render_template('todo.html')
+    if "username" in session:
+        username = session["username"]
+        if request.method == "POST":
+            pass
+        else:
+            return render_template('todo.html', user=username)
+    else:
+        return redirect("/login")
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -47,12 +69,11 @@ def login():
         data = collection.find_one({"username": username})
         datapassword = data["password"]
 
-        #decryption not working
-        decryptedPassword = fernet.decrypt(datapassword).decode()
-
-        if password != decryptedPassword:
+        if password != datapassword:
             flash("The password is incorrect", category="error")
             return redirect(request.url)
+
+        session["username"] = username
 
         return redirect("/todo")
         
@@ -61,8 +82,6 @@ def login():
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
-    global fernet
-
     if request.method == "POST":
         username = request.form.get('username')
         password = request.form.get('password')
@@ -79,9 +98,8 @@ def signup():
         if not collection.find_one({"username": username}) == None:
             flash("username already exsits", category="error")
             return redirect(request.url)
-        
-        hashedPassword = fernet.encrypt(password.encode())
-        collection.insert_one({"username": username, "password": hashedPassword, "todo": []})
+
+        collection.insert_one({"username": username, "password": password, "todo": []})
 
         return redirect("/login")
     else:
